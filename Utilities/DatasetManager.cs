0e109dff0cf1553ca062a724a68b607674b5da75
Tonitete <antonio.caballero.miro@gmail.com>
2024-03-29 02:57:22 +0100

    Added documentation to DatasetManager

--- Utilities/DatasetManager.cs
+++ Utilities/DatasetManager.cs
@@ -14,6 +14,10 @@ namespace FakeApi.Utilities
             _env = env;
         }
 
+        /// <summary>
+        /// Retrieves all products from the dataset.
+        /// </summary>
+        /// <returns>A list of ProductModel objects representing the products.</returns>
         public List<ProductModel> GetAllProducts()
         {
             var webRoot = _env.WebRootPath;
@@ -34,13 +38,23 @@ namespace FakeApi.Utilities
 
         }
 
-        public ProductModel? GetProductById (string id)
+        /// <summary>
+        /// Retrieves a product model from the database based on the provided ID.
+        /// </summary>
+        /// <param name="id">The ID of the product to retrieve.</param>
+        /// <returns>The product model with the specified ID, or null if no product is found.</returns>
+        public ProductModel? GetProductById(string id)
         {
             var products = GetAllProducts();
             var product = products.Where(m => m.Id == id).FirstOrDefault();
             return product;
         }
 
+        /// <summary>
+        /// Retrieves a list of ProductModel objects filtered by the specified category ID.
+        /// </summary>
+        /// <param name="categoryId">The ID of the category to filter by.</param>
+        /// <returns>A list of ProductModel objects that belong to the specified category.</returns>
         public List<ProductModel> GetByCategory(string categoryId)
         {
             var products = GetAllProducts();
@@ -48,6 +62,11 @@ namespace FakeApi.Utilities
             return filteredProducts.ToList();
         }
 
+
+        /// <summary>
+        /// Retrieves all categories from the dataset file.
+        /// </summary>
+        /// <returns>A list of CategoryModel objects representing the categories.</returns>
         public List<CategoryModel> GetAllCategories()
         {
             var webRoot = _env.WebRootPath;
@@ -63,7 +82,12 @@ namespace FakeApi.Utilities
                 throw new Exception(ex.Message);
             }
         }
-        
+
+        /// <summary>
+        /// Retrieves a CategoryModel object from the database based on the provided ID.
+        /// </summary>
+        /// <param name="id">The ID of the category to retrieve.</param>
+        /// <returns>The CategoryModel object with the specified ID, or null if no category is found.</returns>
         public CategoryModel? GetCategoryById(int id)
         {
             var categories = GetAllCategories();
@@ -71,6 +95,11 @@ namespace FakeApi.Utilities
             return category;
         }
 
+        /// <summary>
+        /// Retrieves a CategoryModel object from the list of all categories based on the provided name.
+        /// </summary>
+        /// <param name="name">The name of the category to retrieve.</param>
+        /// <returns>The CategoryModel object with the specified name, or null if no category with that name exists.</returns>
         public CategoryModel? GetCategoryByName(string name)
         {
             var categories = GetAllCategories();
